7f7238e0ef4aa89a70f151a6251e776a0f770dc5
9991rendy <118433448+9991rendy@users.noreply.github.com>
2022-11-24 16:13:19 +0700

    Add files via upload

---
+++ 1_D_71220889.py
@@ -0,0 +1,11 @@
+print("selamat datang di kalkulator sederhana.")
+print("ketik 1 untuk menghitung penjumblahan.")
+print("ketik 2 untuk menghitung pengurangan.")
+print("ketik 3 untuk menghitung perkalian.")
+print("ketik 4 untuk menghitung pembagian.")
+print("ketik 5 untuk menghitung sisa hasil bagi(modulus).")
+print("ketik 6 untuk menghitung pemangkatan.")
+pilihan = input("masukan pilihan anda :")
+print = eval(input("masukan bilangan pertama :"))
+print = input("masukan bilangan kedua :")
+
