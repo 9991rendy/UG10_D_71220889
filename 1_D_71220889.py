6208a58c488f2c8ebbf00189dcd70ea3cfe11d4a
9991rendy <118433448+9991rendy@users.noreply.github.com>
2022-11-24 17:07:50 +0700

    Add files via upload

--- 1_D_71220889.py
+++ 1_D_71220889.py
@@ -1,11 +1,28 @@
-print("selamat datang di kalkulator sederhana.")
-print("ketik 1 untuk menghitung penjumblahan.")
-print("ketik 2 untuk menghitung pengurangan.")
-print("ketik 3 untuk menghitung perkalian.")
-print("ketik 4 untuk menghitung pembagian.")
-print("ketik 5 untuk menghitung sisa hasil bagi(modulus).")
-print("ketik 6 untuk menghitung pemangkatan.")
-pilihan = input("masukan pilihan anda :")
-print = eval(input("masukan bilangan pertama :"))
-print = input("masukan bilangan kedua :")
-
+print("Ketik 1 untuk menghitung penjumlahan")
+print("Ketik 2 untuk menghitung pengurangan")
+print("Ketik 3 untuk menghitung perkalian")
+print("Ketik 4 untuk menghitung pembagian")
+print("Ketik 5 untuk menghitung sisa hasil bagi(modulus")
+print("Ketik 6 untuk menghitung pemangkatan")
+p = int(input("masukkan pilihan anda: "))
+q = int(input("masukkan bilangan pertama: "))
+r = int(input("masukkan bilangan kedua: "))
+if p == 1 :
+    jumlah = q + r
+    print("hasil dari",q,"dijumlahkan dengan",r,"adalah",jumlah)
+elif p == 2:
+    jumlah = q - r
+    print("hasil dari",q,"dikurangi dengan",r,"adalah",jumlah)
+elif p == 3 :
+    jumlah = q * r
+    print("hasil dari",q,"dikali dengan",r,"adalah",jumlah)
+elif p == 4 :
+    jumlah = q/r
+    print("hasil dari",q,"dibagi dengan",r,"adalah",jumlah)
+elif p == 5 :
+    jumlah = q%r
+    print("hasil dari",q,"modulo dari",r,"adalah",jumlah)
+elif p == 6 :
+    jumlah = q**r
+    print("hasil dari",q,"pangkat dengan",r,"adalah",jumlah)
+    
